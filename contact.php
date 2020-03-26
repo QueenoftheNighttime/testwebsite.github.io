0b4b8404ab112f74bf3111f1f9331d8820d10099
QueenoftheNighttime <62692753+QueenoftheNighttime@users.noreply.github.com>
2020-03-26 09:35:52 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Shift</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="stylecontact.css">
+  <link href="https://fonts.googleapis.com/css?family=Orbitron:500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Orbitron:700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <div id="logo">
+      <a href="#">
+        <img src="https://cdn.discordapp.com/attachments/651417452772589569/692423511695818895/shift_img_fixed.jpg" alt="Logo">
+      </a>
+    </div>
+
+    <nav id="main-nav">
+      <ul>
+        <li> <a href="homepage.html">Home</a> </li>
+        <li> <a href="explore.html">Explore</a> </li>
+        <li> <a href="#work">Work</a> </li>
+        <li> <a href="#contact">Contact</a> </li>
+      </ul>
+    </nav>
+  </header>
+
+  <section id="home">
+    <hr>
+    <h1>Contact</h1>
+    <form>
+      <input class="input_text" type="email" tabindex="1" placeholder="E-mail"><br>
+      <input class="input_text" type="subject" tabindex="2" placeholder="Subject"><br>
+      <textarea name='message' placeholder="Message"></textarea><br>
+      <input class="button" type="submit">
+    </form>
+  </section>
+  <footer>
+  <p>
+    &copy; Hush.lol
+  </p>
+</footer>
+</body>
